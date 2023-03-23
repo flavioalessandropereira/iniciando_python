a005d6532c4fc8df4d4fc67d31a4113d34ced748
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-03-23 08:39:28 -0300

    jokenpô

--- jokenpo.py
+++ jokenpo.py
@@ -64,13 +64,3 @@ elif computador == 2:
       print('COMPUTADOR VENCE')
     elif jogador == 2:
        print( 'EMPATE')
-
-
-nome = input('Qual seu nome? ')
-
-print('Prazer em te conhecer {}!'.format(nome))
-
-print('Prazer em te conhecer {:>20}!'.format(nome))
-print('Prazer em te conhecer {:<20}!'.format(nome))
-print('Prazer em te conhecer {:20}!'.format(nome))
-print('Prazer em te conhecer {:^20}!'.format(nome))
