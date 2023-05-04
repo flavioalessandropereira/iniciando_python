af4bbfab1b4815f10a82c7f86e49f38f0a7e526a
flavioalessandropereira <flavioalessandropereira@gmail.com>
2023-05-04 14:51:24 -0300

    dominando_string

--- dominando_string.py
+++ dominando_string.py
@@ -4,3 +4,18 @@ nome = "flavIO"
 print(nome.upper()) # tudo em maiúsculo
 print(nome.lower()) # tudo em minúsuculo
 print(nome.title()) # primeira letra em maiúscula
+
+
+texto = "   ola, mundo!   "
+
+print (texto)
+print(texto.strip() + ".") # tira os espaços em brancos
+print(texto.lstrip() + ".") #t ira os espaços em brancos da esquerda
+print(texto.rstrip() + ".") # tira os espaços em brancos da direita
+
+menu = "Python"
+
+print('####' + menu + '####')
+
+print(menu.center(14)) #centraliza o texto em 14 espços
+print(menu.center(14, '#')) #centraliza o texto e coloca o carater # no lugar do espaço
